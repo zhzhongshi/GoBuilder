5a8397bb10e0b7c1e45cec0401a70ab213a459cc
空城 <3136106883@qq.com>
2021-02-04 15:03:39 +0800

    I'm not happy with the code, so I modified it.

--- internal/app/constants.go
+++ internal/app/constants.go
@@ -3,11 +3,13 @@ package app
 import "os"
 
 // GoBuilderDataDir is the name of GoBuilder's Data Directory
-const GoBuilderDataDir = "gobuilder"
-const GoBuilderExecutableDir = "gobuilder/.executable"
-const GoBuilderExecutableDirKeepFile = "gobuilder/.executable/.keep"
-const GoBuilderTasksDir = "gobuilder/tasks"
-const GoBuilderTasksDirKeepFile = "gobuilder/tasks/.keep"
-const GoBuilderCommandFile = "gobuilder/cmd.gb"
-const DefaultProjectPath = "./"
-const DefaultPerm os.FileMode = 777
+const (
+	GoBuilderDataDir                           = "gobuilder"
+	GoBuilderExecutableDir                     = "gobuilder/.executable"
+	GoBuilderExecutableDirKeepFile             = "gobuilder/.executable/.keep"
+	GoBuilderTasksDir                          = "gobuilder/tasks"
+	GoBuilderTasksDirKeepFile                  = "gobuilder/tasks/.keep"
+	GoBuilderCommandFile                       = "gobuilder/cmd.gb"
+	DefaultProjectPath                         = "./"
+	DefaultPerm                    os.FileMode = 777
+)
